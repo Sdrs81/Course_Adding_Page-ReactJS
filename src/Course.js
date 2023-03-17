2e29e681b7a913af139191608978003f100dc330
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-17 19:02:23 +0300

    Images have been added and Course component has been created

---
+++ src/Course.js
@@ -0,0 +1,12 @@
+import Angular from './images/angular.jpg'
+import Bootstrap from './images/bootstrap5.png'
+import Ccsahrp from './images/ccsharp.png'
+import KompleWeb from './images/kompleweb.jpg'
+
+function Course() {
+    return ( <div>
+
+    </div> );
+}
+
+export default Course;
