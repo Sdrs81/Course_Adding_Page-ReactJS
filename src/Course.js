c5a727d90aa4d8420782df13bc8707d58e1aacae
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-17 19:33:48 +0300

    Necessary functions have been added

--- src/Course.js
+++ src/Course.js
@@ -1,11 +1,12 @@
 import Angular from './images/angular.jpg'
 import Bootstrap from './images/bootstrap5.png'
-import Ccsahrp from './images/ccsharp.png'
+import Ccsharp from './images/ccsharp.png'
 import KompleWeb from './images/kompleweb.jpg'
 
-function Course() {
+function Course({courseName}) {
+    console.log(courseName)
     return ( <div>
-
+        <img src={Angular} alt="" />
     </div> );
 }
 
